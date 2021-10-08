87dde6bb2ed7956ce56b3ab3ed4a50c1e48eff1f
Neciu Dan <cst2989@gmail.com>
2021-10-08 15:06:59 +0200

    Added working kata for bowling

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,9 +1,234 @@
-import { greet } from './main';
+import { frame, totalScore, game } from './main';
 
-test('the data is peanut butter', () => {
-  expect(1).toBe(1);
+describe('Game function', () => {
+  test('it should return 0 for only zero rolls', () => {
+    const gameScore = game([
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+    ]);
+    expect(gameScore).toBe(0);
+  });
+  test('it should return correct score when last frame is a strike', () => {
+    const gameScore = game([
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      10,
+      0,
+      5,
+      2,
+    ]);
+    expect(gameScore).toBe(17);
+  });
+  test('it should return correct score when last frame is a spare', () => {
+    const gameScore = game([
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      5,
+      5,
+      2,
+    ]);
+    expect(gameScore).toBe(12);
+  });
+
+  test('All strikes should have correct score ', () => {
+    const gameScore = game([
+      10,
+      0,
+      10,
+      0,
+      10,
+      0,
+      10,
+      0,
+      10,
+      0,
+      10,
+      0,
+      10,
+      0,
+      10,
+      0,
+      10,
+      0,
+      10,
+      10,
+      10,
+    ]);
+    expect(gameScore).toBe(300);
+  });
+  test('Complex numbers ', () => {
+    const gameScore = game([
+      5,
+      4, // 9
+      8,
+      2, // S  -- 29
+      10,
+      0, // X -- 50
+      10,
+      0, // X -- 61
+      1,
+      0, // 1 -- 62
+      9,
+      1, // S -- 72
+      0,
+      10, // S -- 92
+      10,
+      0, // X -- 112
+      6,
+      4, // S -- 129
+      7,
+      3, // S -- 149
+      10,
+    ]);
+    expect(gameScore).toBe(149);
+  });
 });
 
-test('greeting', () => {
-  expect(greet('Foo')).toBe('Hello Foo');
+describe('Frame function', () => {
+  test('it should return the correct score after two rolls', () => {
+    const score = frame(2, 3);
+    expect(score).toBe(5);
+  });
+
+  test('it should return the S sign if the players hit a spare', () => {
+    const score = frame(3, 7);
+    expect(score).toBe('S');
+  });
+
+  test('it should return the X sign if the players hit a strike on the first roll', () => {
+    const score = frame(10, 0);
+    expect(score).toBe('X');
+  });
+});
+
+describe('Total Score', () => {
+  test('it shoudl correctly calculate the score without spares or strikes', () => {
+    const score = totalScore([
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+    ]);
+    expect(score).toBe(20);
+  });
+
+  test('it shoudl correctly calculate the score with a spare', () => {
+    const score = totalScore([
+      [1, 1],
+      [1, 1],
+      [5, 5],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+    ]);
+    expect(score).toBe(29);
+  });
+
+  test('it shoudl correctly calculate the score with multiple spares', () => {
+    const score = totalScore([
+      [1, 1],
+      [1, 1],
+      [5, 5],
+      [1, 1],
+      [1, 1],
+      [5, 5],
+      [5, 5],
+      [1, 1],
+      [5, 5],
+      [1, 1],
+    ]);
+    expect(score).toBe(60);
+  });
+
+  test('it shoudl correctly calculate the score with a strike', () => {
+    const score = totalScore([
+      [1, 1],
+      [1, 1],
+      [10, 0],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+    ]);
+    expect(score).toBe(30);
+  });
+
+  test('it shoudl correctly calculate the score with multiple strikes', () => {
+    const score = totalScore([
+      [1, 1],
+      [1, 1],
+      [10, 0],
+      [10, 0],
+      [5, 1],
+      [1, 1],
+      [10, 0],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+    ]);
+    expect(score).toBe(71);
+  });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,74 @@
-export const greet = (value: string) => {
-  return 'Hello ' + value;
+// The game consists of 10 frames. In each frame the player has two rolls to knock down 10 pins.
+// The score for the frame is the total number of pins knocked down, plus bonuses for strikes and spares.
+
+// A spare is when the player knocks down all 10 pins in two rolls.
+// The bonus for that frame is the number of pins knocked down by the next roll.
+
+// A strike is when the player knocks down all 10 pins on his first roll.
+// The frame is then completed with a single roll.
+// The bonus for that frame is the value of the next two rolls.
+// In the tenth frame, a player who rolls a spare or strike is allowed to roll the extra balls to complete the frame.
+// However no more than three balls can be rolled in the tenth frame
+
+export const frame = (rollOne: number, rollTwo: number): number | string => {
+  if (rollOne === 10) {
+    return 'X';
+  }
+  let score = rollOne + rollTwo;
+  return score === 10 ? 'S' : score;
+};
+
+export const totalScore = (FrameScoreCard: [number, number][]): number => {
+  let scoreArray: any = [];
+  let score: number = 0;
+  FrameScoreCard.forEach((frameRoll) => {
+    const frameScore = frame(frameRoll[0], frameRoll[1]);
+    scoreArray.push(frameScore);
+  });
+
+  for (let index = 0; index < 10; index++) {
+    let roundScore = scoreArray[index];
+    if (roundScore === 'S') {
+      // handle Spare
+      roundScore = 10 + FrameScoreCard[index + 1][0];
+    }
+    if (roundScore === 'X') {
+      // handle Strike
+
+      const futurePoint = FrameScoreCard[index + 2]
+        ? FrameScoreCard[index + 2][0]
+        : 10;
+      const nextPoint =
+        FrameScoreCard[index + 1][1] || FrameScoreCard[index + 1][0] !== 10
+          ? FrameScoreCard[index + 1][1]
+          : futurePoint;
+      roundScore = 10 + FrameScoreCard[index + 1][0] + nextPoint;
+    }
+    score = score + parseInt(roundScore);
+  }
+
+  return score;
+};
+
+export const game = (Rolls: number[]): number => {
+  const FrameScoreCard = listToMatrix(Rolls);
+  // verify the end of the game
+  return totalScore(FrameScoreCard);
+};
+
+const listToMatrix = (list: number[]) => {
+  let matrix: any = [],
+    i,
+    k;
+
+  for (i = 0, k = -1; i < list.length; i++) {
+    if (i % 2 === 0) {
+      k++;
+      matrix[k] = [];
+    }
+
+    matrix[k].push(list[i]);
+  }
+
+  return matrix;
 };
